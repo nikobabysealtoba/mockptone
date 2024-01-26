8796bdcc0f1293b6a60c2cd8c1b6c7875933b775
nikobabysealtoba <156109799+nikobabysealtoba@users.noreply.github.com>
2024-01-26 12:30:07 -0600

    dialogue most recent

--- main.ts
+++ main.ts
@@ -3,12 +3,16 @@ namespace SpriteKind {
 }
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     if (controller.player2.isPressed(ControllerButton.B)) {
-        game.splash("")
+        tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(location.column, location.row + 1))
+        the_call()
+        story.startCutscene(function () {
+            story.printCharacterText("", "PHONE")
+        })
     }
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.droppeditem, function (sprite, otherSprite) {
     if (intro == false && playerspawned == 0 && controller.B.isPressed()) {
-        timer.after(0, function () {
+        timer.after(500, function () {
             if (intro == false && playerspawned == 0 && controller.B.isPressed()) {
                 itemheld = true
                 helditemsprite = otherSprite.image
@@ -31,6 +35,34 @@ function spriterotate () {
     angle = spriteutils.radiansToDegrees(angle)
     rotationalsprite.setImage(scaling.rot(originalimage.clone(), angle))
 }
+function the_call () {
+    game.splash("You have one new message!", "You have one new message!")
+    normal_hp = game.ask("Pick up phone?")
+    if (normal_hp == true) {
+        game.splash("The phone starts blaring. ")
+    } else {
+        game.splash("The phone starts blaring anyway, your heart feels heavy.")
+    }
+    message_roll = 1
+    if (message_roll == 1) {
+        story.startCutscene(function () {
+            controller.moveSprite(rotationalsprite, 0, 0)
+            story.printCharacterText("Hi! This is 'Dunvan' from O'Donald's Auto Parts", "PHONE")
+            story.printCharacterText("We have an order ready for you.", "PHONE")
+            story.printCharacterText("What city would you like this picked up at?", "PHONE")
+            story.showPlayerChoices("HONOLULU.", "HILO.", "HULUALOA.")
+            map_size = story.getLastAnswer()
+            story.printCharacterText("Would you like to collect your package outside?", "PHONE")
+            story.showPlayerChoices("YES.", "NO.")
+            outsideornah = story.getLastAnswer()
+            story.printCharacterText("Would you like to collect your package outside?", "PHONE")
+        })
+    } else if (message_roll == 2) {
+    	
+    } else if (message_roll == 3) {
+    	
+    }
+}
 scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
     sprites.destroy(sprite)
 })
@@ -46,30 +78,12 @@ function intro2 () {
     textSprite2.setScale(0.8, ScaleAnchor.Bottom)
     textSprite2.setPosition(76, 76)
     textSprite2.setOutline(1, 15)
-}
-function playersetup () {
-    playerspawned = 0
-    angle = 0
-    originalimage = assets.image`myImage`
-    itemheld = false
-    rotationalsprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
-    rotationalsprite.setStayInScreen(true)
-    controller.moveSprite(rotationalsprite)
-    crowbar = sprites.create(assets.image`crowbar`, SpriteKind.droppeditem)
-    bat = sprites.create(assets.image`enemy gun`, SpriteKind.droppeditem)
-    scene.cameraFollowSprite(rotationalsprite)
     Mouse.DrawMouse(
     true,
     assets.image`secondmousesmall`,
     0,
     0
     )
-    scene.setBackgroundImage(assets.image`a`)
-    scene.setBackgroundColor(7)
-    tiles.setCurrentTilemap(tilemap`starting area`)
-    tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(15, 30))
-    tiles.placeOnTile(bat, tiles.getTileLocation(21, 9))
-    tiles.placeOnTile(crowbar, tiles.getTileLocation(21, 11))
     Mouse.Setsensibility(1.33334)
     Keybinds.setSimulatorKeymap(
     Keybinds.PlayerNumber.ONE,
@@ -90,25 +104,41 @@ function playersetup () {
     Keybinds.CustomKey.E
     )
 }
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    let testenemy: Sprite = null
-    sprites.destroy(projectile)
-    testenemy.setImage(assets.image`dumb smile`)
-    testenemy.follow(rotationalsprite, 0)
-})
+function playersetup () {
+    playerspawned = 0
+    angle = 0
+    originalimage = assets.image`myImage`
+    itemheld = false
+    rotationalsprite = sprites.create(assets.image`myImage`, SpriteKind.Player)
+    rotationalsprite.setStayInScreen(true)
+    controller.moveSprite(rotationalsprite)
+    crowbar = sprites.create(assets.image`crowbar`, SpriteKind.droppeditem)
+    bat = sprites.create(assets.image`enemy gun`, SpriteKind.droppeditem)
+    scene.cameraFollowSprite(rotationalsprite)
+    scene.setBackgroundImage(assets.image`a`)
+    scene.setBackgroundColor(7)
+    tiles.setCurrentTilemap(tilemap`starting area`)
+    tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(15, 30))
+    tiles.placeOnTile(bat, tiles.getTileLocation(21, 9))
+    tiles.placeOnTile(crowbar, tiles.getTileLocation(21, 11))
+}
 let mouseanimationcycle = 0
 let bat: Sprite = null
 let crowbar: Sprite = null
 let textSprite2: TextSprite = null
 let textSprite: TextSprite = null
-let rotationalsprite: Sprite = null
+let outsideornah = ""
+let map_size = ""
+let message_roll = 0
+let normal_hp = false
 let projectile: Sprite = null
 let helditemsprite: Image = null
 let itemheld = false
 let playerspawned = 0
 let intro = false
-let angle = 0
+let rotationalsprite: Sprite = null
 let originalimage: Image = null
+let angle = 0
 intro2()
 forever(function () {
     if (intro == true) {
