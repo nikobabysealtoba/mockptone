2072d22ec41bba7aeb5bc6499c40534f998a9705
alytas <156123368+alytas@users.noreply.github.com>
2024-01-31 11:32:38 -0600

    TILES

--- main.ts
+++ main.ts
@@ -5,7 +5,7 @@ namespace SpriteKind {
 scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile0`, function (sprite, location) {
     if (controller.player2.isPressed(ControllerButton.B)) {
         tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(location.column, location.row + 1))
-        tiles.setTileAt(location, assets.tile`transparency16`)
+        tiles.setTileAt(location, assets.tile`myTile3`)
         the_call(game.ask("Pick up phone?"), randint(1, 3))
     }
 })
@@ -149,6 +149,16 @@ function the_call (_1hpmode: boolean, messageroll: number) {
             enemyspawnblockamount = enemyspawnblocklist.length
             for (let index = 0; index < enemyspawnblockamount; index++) {
                 spawn_enemy()
+            }
+        } else {
+            if (map_size == 1) {
+                tiles.setCurrentTilemap(tilemap`level2`)
+                tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(61, 99))
+                enemyspawnblocklist = tiles.getTilesByType(sprites.dungeon.collectibleInsignia)
+                enemyspawnblockamount = enemyspawnblocklist.length
+                for (let index = 0; index < enemyspawnblockamount; index++) {
+                    spawn_enemy()
+                }
             }
         }
     })
@@ -359,6 +369,10 @@ function playersetup () {
     tiles.placeOnTile(bat, tiles.getTileLocation(21, 9))
     tiles.placeOnTile(crowbar, tiles.getTileLocation(21, 11))
 }
+scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.doorOpenNorth, function (sprite, location) {
+    tiles.setCurrentTilemap(tilemap`level2`)
+    tiles.placeOnTile(rotationalsprite, tiles.getTileLocation(61, 99))
+})
 let mouseanimationcycle = 0
 let enemyswing = 0
 let bat: Sprite = null
